f9c732c666e527914555ff6473355196f1250115
Chris <103796740+ChrisA98@users.noreply.github.com>
2023-07-12 18:04:16 -0400

    worked on beam attack

--- Project/Parent_Scenes/Scripts/Attack_Beam.gd
+++ Project/Parent_Scenes/Scripts/Attack_Beam.gd
@@ -1,5 +1,5 @@
 @tool
-extends ShapeCast3D
+extends Node3D
 
 @export var beam_data : Beam_Data
 @export var test_trgt : Vector3
@@ -8,12 +8,14 @@ var damage: float
 var damage_type: String
 var target_location: Vector3
 
+@onready var beams = [$Attack_Beam]
+
 func _ready():
 	pass
 
 
 func _process(_delta):
-	_create_beam(test_trgt)
+	_create_beam(beams[0],test_trgt,true)
 
 ## Fire beam
 func fire(dmg:float, dmg_type:String, trgt:Vector3):
@@ -21,9 +23,10 @@ func fire(dmg:float, dmg_type:String, trgt:Vector3):
 	damage_type = dmg_type
 	target_location = trgt
 	
-	_create_beam(target_location)
+	_create_beam(beams[0],target_location)
+	beams[0].force_shapecast_update()
 	
-	enabled = true	
+	beams[0].enabled = true	
 
 
 ## End Beam early
@@ -32,22 +35,24 @@ func end_beam():
 
 
 ## Create Beam
-func _create_beam(trgt):
-	## Basic Beam	
-	target_position.z = -(position.distance_to(trgt)/2)
-	shape.size.z = position.distance_to(trgt)
+func _create_beam(_beam,trgt,clear:=false):
+	if clear:
+		for i in range(1,beams.size()-1):
+			beams[i].free()
+			
+	## Basic Beam
 	look_at(trgt,Vector3.UP)
-	force_shapecast_update()
+	_set_beam(_beam,trgt)
 	
-	if beam_data.penetration == -1:
-		## Dont worry about penetrations
+	if beam_data.penetration == -1 or beams.size() >= beam_data.penetration or _beam.get_collision_count() < 0:
+		## Dont worry about penetrations\
 		return
-	## Worry about Penetrations
-	while get_collision_count() > beam_data.penetration:
-		trgt = get_collision_point(get_collision_count()-1)
-		target_position.z = -(position.distance_to(trgt)/2)
-		shape.size.z = position.distance_to(trgt)
-		look_at(trgt,Vector3.UP)
-		force_shapecast_update()
-	
+	var b_new = _beam.duplicate()
+	_beam.position = _beam.get_collision_point(0)
+	beams.push_back(b_new)
+	_create_beam(b_new,trgt)
 
+func _set_beam(_beam, trgt):
+	_beam.target_position = trgt
+	_beam.force_shapecast_update()
+	
